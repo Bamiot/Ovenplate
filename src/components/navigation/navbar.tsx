76bcbae892d250cb9b3a547a7c2b97b51669aed2
Louis CORD'HOMME <louis.cordhomme@gmail.com>
2024-06-22 08:19:51 +0200

    remove navlink

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -5,14 +5,41 @@ import {
   NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuList,
+  NavigationMenuLink,
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from '@/components/shadcn/navigation-menu'
 import { cn } from '@/lib/utils'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import { useDictionary } from '@/localization'
-import { NavLink } from './navLink'
 import { ThemeToggle } from '../ui/themeToggle'
 import { LangSelect } from '../ui/langSelect'
+import { getPathnameWithoutLocale, getLocale } from '@/lib/localPath'
+
+type NavLinkProps = {
+  href: string
+  children: React.ReactNode
+}
+
+const NavLink = (props: NavLinkProps) => {
+  const pathNameWithoutLocale = getPathnameWithoutLocale(usePathname())
+  const locale = getLocale(usePathname())
+  return (
+    <Link href={`/${locale}${props.href}`} passHref>
+      <NavigationMenuLink
+        asChild={true}
+        className={cn(
+          '!text-lg',
+          pathNameWithoutLocale === props.href && '!font-extrabold',
+          'mx-2'
+        )}
+      >
+        <p>{props.children}</p>
+      </NavigationMenuLink>
+    </Link>
+  )
+}
 
 export function Navbar() {
   const { home } = useDictionary()
@@ -25,6 +52,7 @@ export function Navbar() {
         'border rounded m-4 p-3'
       )}
     >
+      <span />
       <NavigationMenu>
         <NavigationMenuList>
           <NavigationMenuItem>
